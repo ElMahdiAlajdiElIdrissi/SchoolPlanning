b92cb1365102d90daaea388f3d3aadc820ac7f80
Paul <paul_gerarts@hotmail.com>
2019-05-30 18:03:05 +0200

    linked the txt-files from SQL to be started from application

--- src/BackEndCode.java
+++ src/BackEndCode.java
@@ -1,27 +1,37 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
+import java.util.List;
 import java.util.Scanner;
 
 public class BackEndCode {
     private static int counter = 0;
-    public static void main(String[] args) throws SQLException {
-        final String URL = "jdbc:mysql://noelvaes.eu/StudentDB?serverTimezone=UTC";//"jdbc:mysql://localhost/SchoolPlanner?serverTimezone=UTC";
-        final String USERNAME = "student";//"root";
-        final String PASSWORD = "student123";//"Desrath1990";
-        final String sortOnWhat = dataInput();
 
-        try (
-                Connection conn = DriverManager.getConnection(URL,
-                        USERNAME, PASSWORD);
-                Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-                ResultSet result = statement.executeQuery(sortData(sortOnWhat))
-        ) {
+    public static void main(String[] args) throws SQLException{
+        databaseConnection("databasescript.txt");
+    }
 
-            while (result.next()) {
-                System.out.println(result.getString(sortOnWhat));
+    public static void databaseConnection(String textFile){
+        final String URL = "jdbc:mysql://localhost/SchoolPlanner?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "Desrath1990";
+        final String SCRIPT = databaseStartUp(textFile);
+
+        String[] queries = SCRIPT.split(";");
+        for (int i = 0; i < queries.length; i++) {
+            //System.out.println(queries[i].concat(";"));
+            int firstIndex = SCRIPT.indexOf(queries[i].concat(";"));
+            int lastIndex = queries[i].length() + firstIndex;
+            try (
+                    Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                    PreparedStatement statement1 = conn.prepareStatement(SCRIPT.substring(firstIndex, lastIndex).concat(";"));
+            ) {
+                statement1.execute();
+            } catch (SQLException se) {
+                se.printStackTrace();
             }
-        } catch (
-                SQLException se) {
-            se.printStackTrace();
         }
     }
 
@@ -38,13 +48,28 @@ public class BackEndCode {
                     counter++;
                     break;
             }
-            } else {
+        } else {
             counter = 0;
         }
         return giveBack;
     }
 
+    private static String databaseStartUp(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+            for (String line : lines) {
+                    sb.append(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong while reading your text-file!");
+            System.out.println(e.getMessage());
+        }
+        return sb.toString();
+    }
+
     private static String dataInput(){
+        System.out.println("Please type the columnname of what you'd like sorted");
         Scanner keyboard = new Scanner(System.in);
         return keyboard.nextLine();
     }
---
+++ src/DatabaseUpdates.java
@@ -0,0 +1,9 @@
+import java.sql.*;
+
+public class DatabaseUpdates {
+    public static void main(String[] args) {
+            BackEndCode.databaseConnection("InsertIntoDatabase.txt");
+    }
+}
+
+
