fcaa21d54c42946844040551e90abb2b48013a1e
Paul <paul_gerarts@hotmail.com>
2019-06-01 15:25:14 +0200

    created DAO for all tables, without linking them to workable app

---
+++ src/dao/DocentDao.java
@@ -0,0 +1,81 @@
+package dao;
+
+import java.sql.*;
+
+public class DocentDao {
+    private String url;
+    private String user;
+    private String password;
+
+    public DocentDao() {
+
+    }
+
+    public DocentDao(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Docent getDocentByID(int id) throws SQLException {
+        try (Connection conn = getConnection();
+             PreparedStatement statement = conn.prepareStatement("SELECT * FROM docent WHERE Id = ?")) {
+            statement.setInt(1, id);
+            try (ResultSet result = statement.executeQuery()) {
+                if (result.next()) {
+                    Docent docent = new Docent();
+                    docent.setId(id);
+                    docent.setVoorNaam(result.getString("Voor_Naam"));
+                    docent.setAchterNaam(result.getString("Achter_Naam"));
+                    docent.setDepartementID(result.getInt("Departement_Id"));
+                    return docent;
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException se) {
+            throw new SQLException("Something went wrong");
+        }
+    }
+
+    public void updateDocent(Docent docent) throws SQLException {
+        try (Connection conn = getConnection();
+             PreparedStatement statement = conn.prepareStatement("UPDATE docent SET Voor_Naam=?, Achter_Naam=?, Departement_Id=? WHERE Id=? ")) {
+            statement.setString(1, docent.getVoorNaam());
+            statement.setString(2, docent.getAchterNaam());
+            statement.setInt(3,docent.getDepartementID());
+            statement.setInt(4, docent.getId());
+            statement.executeUpdate();
+        } catch (SQLException se) {
+            throw new SQLException("Something went wrong");
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
---
+++ src/dao/Student.java
@@ -0,0 +1,63 @@
+package dao;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String gebruikersNaam;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGebruikersNaam() {
+        return gebruikersNaam;
+    }
+
+    public void setGebruikersNaam(String gebruikersNaam) {
+        this.gebruikersNaam = gebruikersNaam;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Student{");
+        sb.append("id=").append(id);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", gebruikersNaam='").append(gebruikersNaam).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
