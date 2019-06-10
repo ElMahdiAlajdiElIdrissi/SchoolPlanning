22ff704e2c7337c4160b6c62314dda90057cb850
El Mahdi Alajdi El Idrissi <elmahdi.alajdielidrissi@gmail.com>
2019-06-10 11:49:06 +0200

    Made a new controller for the view GevolgdeCursussen to fix a little issue for showing the courses

--- src/SchoolPlanningController.java
+++ src/SchoolPlanningController.java
@@ -1,13 +1,9 @@
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.*;
 
 public class SchoolPlanningController {
 
@@ -15,62 +11,8 @@ public class SchoolPlanningController {
     private Button gevolgdeCursussen;
 
     @FXML
-    private TextField searchField;
-
-    @FXML
-    private ListView table;
-
-    private StringBuilder enlistedCourses = new StringBuilder();
-
-    @FXML
     private void clickedOnGevolgdeCursussen(ActionEvent ev) throws IOException {
-        DataBase db = new DataBase();
-        try(Connection conn = DriverManager.getConnection(db.getURL(), db.getUSERNAME(), db.getPASSWORD());
-            PreparedStatement stm = conn.prepareStatement("SELECT cursus.Naam_Cursus from ingeschrevencursussen join cursus on ingeschrevencursussen.Cursus_Id = cursus.Id where Student_Id = ?;")){
-            stm.setInt(1, GlobalVars.getStudentId());
-            ResultSet rs = stm.executeQuery();
-            while(rs.next()){
-                enlistedCourses.append(rs.getString(1) + " ");
-//                System.out.println(rs.getString(1));
-            }
-        }catch(SQLException ex){
-            ex.printStackTrace();
-        }
-
         ((Stage) gevolgdeCursussen.getScene().getWindow()).setScene(SceneManager.getGevolgdeCursussenScene());
-    }
-
-    @FXML
-    private void searchDocentOrCursus(ActionEvent event)throws IOException {
-        DataBase db = new DataBase();
-        String search = searchField.getText().trim();
-        String queryDocent = "SELECT DISTINCT Voor_Naam, Achter_Naam, Naam_Cursus, Start_Datum, Eind_Datum FROM Docent " +
-                "INNER JOIN Cursus ON docent.id = cursus.id WHERE Voor_Naam=? OR Achter_Naam=? OR Naam_Cursus=?;";
-
-        try (Connection conn = DriverManager.getConnection(db.getURL(), db.getUSERNAME(), db.getPASSWORD());
-             PreparedStatement st = conn.prepareStatement(queryDocent)) {
-            st.setString(1, search);
-            st.setString(2, search);
-            st.setString(3, search);
-            ResultSet rs = st.executeQuery();
-
-            ObservableList<String> docentAndCursus = FXCollections.observableArrayList();
-            while(rs.next()) {
-                int columnCount = rs.getMetaData().getColumnCount();
-                for (int i = 1; i <= columnCount; i++) {
-                    docentAndCursus.add(rs.getString(i) + " ");
-                }
-            }
-            if (docentAndCursus.isEmpty()) {
-                SceneManager.alertError("The teacher/course you were looking for doesn't exist");
-                docentAndCursus.add("These are the courses you already enlisted in: ");
-                docentAndCursus.add(enlistedCourses.toString());
-            }
-            table.setItems(docentAndCursus);
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 
     @FXML
